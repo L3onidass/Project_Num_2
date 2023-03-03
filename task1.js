25d70db821d81aa8f23760b79d6d6f1b851ede32
Levan Malchasjan <frost2@seznam.cz>
2023-03-03 13:08:12 +0100

    Finished first part of the task

--- task1.js
+++ task1.js
@@ -1,3 +1,4 @@
+// L3van
 const countries = [
   "United States",
   "China",
@@ -20,3 +21,26 @@ const countries = [
   "Thailand",
   "Myanmar",
 ];
+const country = "Egypt";
+
+const checkCountry = function (country, countries) {
+  for (let index = 0; index < countries.length; index++) {
+    if (countries[index] === country) {
+      console.log(
+        `Zadaná krajina ${country} se nachází v seznamu krajin na indexe ${countries.indexOf(
+          country
+        )}`
+      );
+      break;
+    } else if (country == "" || typeof country !== "string") {
+      console.log(`Zadali jste neplatný dotaz: ${country}`);
+      break;
+    } else if (countries.includes(country) === false) {
+      console.log(`Zadaná krajina ${country} se nenachází v seznamu krajin`);
+      break;
+    }
+  }
+  return;
+};
+
+checkCountry(country, countries);
