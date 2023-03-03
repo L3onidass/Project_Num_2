7d8deb3e6459b3b56a20e7817a52321cc4a02900
Levan Malchasjan <frost2@seznam.cz>
2023-03-03 16:11:50 +0100

    Added third part of task1 and extras

--- task1.js
+++ task1.js
@@ -22,9 +22,7 @@ const countries = [
   "Myanmar",
 ];
 
-// *****First part*****
-
-const country = prompt("Zadejte název krajiny k vyhledání");
+// *****First part - Check function*****
 
 const checkCountry = function (country, countries) {
   for (let index = 0; index < countries.length; index++) {
@@ -46,7 +44,7 @@ const checkCountry = function (country, countries) {
   return;
 };
 
-// *****Second part*****
+// *****Second part - Add function*****
 
 const addCountry = function (country, countries) {
   for (let index = 0; index < countries.length; index++) {
@@ -72,3 +70,53 @@ const addCountry = function (country, countries) {
   }
   return;
 };
+
+// *****Third part - Remove function *****
+
+const removeCountry = function (country, countries) {
+  for (let index = 0; index < countries.length; index++) {
+    if (countries[index] !== country) {
+      console.log(`Zadaná krajina ${country} se nenachází v seznamu krajin`);
+      break;
+    } else if (country === "" || typeof country !== "string") {
+      console.log(`Zadali jste neplatný dotaz: ${country}`);
+      break;
+    } else if (countries.includes(country) === true) {
+      countries.splice(countries.indexOf(country), 1);
+
+      console.log(
+        `Zadaná krajina ${country} byla odstraněná se seznamu krajin na indexu  ${countries.indexOf(
+          country
+        )}. Aktuální počet krajin v seznamu je ${countries.length}`
+      );
+      console.log(countries);
+      break;
+    }
+  }
+  return;
+};
+
+// ***** Extras *****
+
+const choice = prompt(
+  "Vítejte v aplikaci pro správu krajin. Vyberte si jeden z následujících pokynů: check - kontrola krajiny, add - přidání krajiny, remove - odstranit krajinu."
+);
+
+if (choice === "check") {
+  let country = prompt(
+    "Vybral jste příkaz Check. Zadejte krajinu kterou chcete zkontrolovat."
+  );
+  checkCountry(country, countries);
+} else if (choice === "add") {
+  let country = prompt(
+    "Vybral jste příkaz add. Zadejte krajinu kterou chcete přidat."
+  );
+  addCountry(country, countries);
+} else if (choice === "remove") {
+  let country = prompt(
+    "Vybral jste příkaz remove. Zadejte krajinu kterou chcete odstranit."
+  );
+  removeCountry(country, countries);
+} else {
+  console.log("Vybral jste neplatný pokyn.");
+}
